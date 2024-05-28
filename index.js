a864ee77fbf02b415ce7c43e3365e392f35e8f3f
Nasif Zeehan <85276921+Nasif28@users.noreply.github.com>
2024-05-28 12:21:10 +0600

    all image problem solved

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ limit: '5mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define the allowed origins
-const allowedOrigins = ['http://localhost:5173', 'https://happypawsbd.onrender.com'];
+const allowedOrigins = ['http://localhost:5173', 'https://happypawsbd.onrender.com', 'https://happypawsbd.vercel.app'];
 
 // Configure CORS
 app.use(cors({
